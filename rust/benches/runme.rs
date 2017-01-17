23a9e99a97b81e6551329f5d73ba4d77b06c0ea6
Morten Dahl <mortendahlcs@gmail.com>
2017-01-17 17:22:22 +0100

    update to use public version of paillier lib

--- rust/benches/runme.rs
+++ rust/benches/runme.rs
@@ -4,42 +4,34 @@ extern crate paillier;
 
 use bencher::Bencher;
 use paillier::*;
-use paillier::plain::*;
 
-type Scheme = PlainPaillier;
-
-// pub fn bench_1024(b: &mut Bencher)
-// {
-//     let (ek, dk) = Scheme::keypair(1024);
-//
-//     let m = Scheme::encode(1056287461);
-//
-//     let c = Scheme::encrypt(&ek, &m);
-//     b.iter(|| {
-//         let _ = Scheme::decrypt(&dk, &c);
-//     });
-// }
-
-pub fn bench_2048(b: &mut Bencher)
+pub fn encrypt_decrypt<KS>(b: &mut Bencher)
+where
+    KS: KeySize
 {
-    let (ek, dk) = Scheme::keypair(2048);
+    let (ek, dk) = Paillier::keypair_with_modulus_size(KS::get());
 
-    use paillier::arithimpl::traits::Samplable;
     // let n: BigInteger = str::parse("23601375460155562757123678360900229644381030159964965932095920363097284825175029196457022864038449469086188985762066259059164844287276915193108505099612427967057134520230945630209577834878763915645946525724125804370016991193585261991964913084246563304755455418791629494251095184144084978275430600444710605147457044597210354635288909909182640243950968376955162386281524128586829759108414295175173359174297599533960370415928328418610692822180389889327103292184546896322100484378149887147731744901289563127581082141485046742100147976163228583170704180024449958168221243717383276594270459874555884125566472776234343167371").unwrap();
     // let m = Plaintext(n);
-    let m = Scheme::encode(1056287461);
-
-
+    let m = core::Plaintext::from(1056287461);
 
     b.iter(|| {
-        let c = Scheme::encrypt(&ek, &m);
-        let _ = Scheme::decrypt(&dk, &c);
+        let c = Paillier::encrypt(&ek, &m);
+        let _ = Paillier::decrypt(&dk, &c);
     });
 }
 
+pub trait KeySize { fn get() -> usize; }
+struct KeySize1024; impl KeySize for KeySize1024 { fn get() -> usize { 1024 } }
+struct KeySize2048; impl KeySize for KeySize2048 { fn get() -> usize { 2048 } }
+struct KeySize3072; impl KeySize for KeySize3072 { fn get() -> usize { 3072 } }
+struct KeySize4096; impl KeySize for KeySize4096 { fn get() -> usize { 4096 } }
+
 benchmark_group!(group,
-    // bench_1024,
-    bench_2048
+    encrypt_decrypt<KeySize1024>,
+    encrypt_decrypt<KeySize2048>,
+    encrypt_decrypt<KeySize3072>,
+    encrypt_decrypt<KeySize4096>
 );
 
 benchmark_main!(group);
