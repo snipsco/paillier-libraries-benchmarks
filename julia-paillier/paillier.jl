c86b03650f508428dcf1a027bda98bdbfe866cfa
Alaa Saade <alaa.saade@snips.ai>
2017-01-19 17:02:49 +0100

    add decryption in paillier.jl

--- julia-paillier/paillier.jl
+++ julia-paillier/paillier.jl
@@ -1,3 +1,8 @@
+type PrivateEncryptionKey
+    l::BigInt
+    m::BigInt
+end
+
 type PublicEncryptionKey
     n::BigInt
     n_sq::BigInt
@@ -7,10 +12,21 @@ end
 PublicEncryptionKey(n) = PublicEncryptionKey(n,n^2,n+1)
 PublicEncryptionKey(p,q) = PublicEncryptionKey(p*q)
 
+function PrivateEncryptionKey(p,q,n)
+    l = (p-1)*(q-1)
+    m = invmod(l, n)
+    PrivateEncryptionKey(l,m)
+end
+
 function encrypt(pub::PublicEncryptionKey, m)
     rng = RandomDevice()
     r = rand(rng, 2:pub.n)::BigInt
     rn = powermod(r, pub.n, pub.n_sq)
     gm = mod( (pub.n * m) + 1, pub.n_sq )
     c = mod(gm * rn, pub.n_sq)
+end
+
+function decrypt(priv, pub, cipher)
+    x = powermod(cipher, priv.l, pub.n_sq) - 1
+    plain = mod(div(x, pub.n) * priv.m, pub.n)
 end
